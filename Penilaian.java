56d5b8a15ad9e5a8da9dc6f98d25f111c8c6229f
aiska28 <aiskacaca8@gmail.com>
2025-04-21 17:07:26 +0700

    fix banget ini teh

--- Penilaian.java
+++ Penilaian.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 public class Penilaian {
-    Mahasiswa[] mhs = Mahasiswa.dataMahasiswa;
-    MataKuliah[] mk = MataKuliah.dataMatakuliah;
+    Mahasiswa[] mhs = Mahasiswa.daftarMahasiswa;
+    MataKuliah[] mk = MataKuliah.dataMataKuliah;
 
     Mahasiswa[] mahasiswaPenilaian = {
         mhs[0], mhs[0], mhs[1], mhs[2],mhs[2] 
     };
 
     MataKuliah[]mataKuliahPenilaian = {
-        mk[0], mk[0], mk[1], mk[1] };
+        mk[0], mk[0], mk[1], mk[1] 
+    };
     
         double[] nilaiTugas = {80, 60, 75, 85, 80};
         double[] nilaiUTS = {85, 75, 70, 90, 90};
@@ -37,23 +38,41 @@ public class Penilaian {
             }
         }
 
-    public void urutkanNilaiAkhir(){
-        System.out.println("\nData penilaian setelah diurutkan: ");
-        int n = mahasiswaPenilaian.length;
-        int[] urutan = new int[n];
-        for (int i = 0; i < n; i++) urutan[i] = i;
+        public void urutkanNilaiAkhir(){
+            System.out.println("\nData penilaian setelah diurutkan: ");
+            int n = mahasiswaPenilaian.length;
+            int[] urutan = new int[n];
+                for (int i = 0; i < n; i++) urutan[i] = i;
 
-        for(int i = 0; i < n -1; i++){
-            for(int j = 0; j < n-1; i++){
-                if(hitungNilaiAkhir(urutan[j]) < hitungNilaiAkhir(urutan[j + 1])) {
-
-                    for(int i : urutan) {
-                        double nilaiAkhir = hitungNilaiAkhir(i);
-                        System.out.println(mahasiswaPenilaian[i].nama + " | " + 
-                                            mataKuliahPenilaian[i].namaMK + " | Nilai Akhir: " + nilaiAkhir );
+                for(int i = 0; i < n -1; i++){
+                    for(int j = 0; j < n-1; i++){
+                        if(hitungNilaiAkhir(urutan[j]) < hitungNilaiAkhir(urutan[j + 1])) {
+                        int temp = urutan[j];
+                        urutan[j] = urutan [j+1];
+                        urutan [j+1] = temp;
+            
                     }
-
                 }
             }
+            for(int i : urutan) {
+                double nilaiAkhir = hitungNilaiAkhir(i);
+                System.out.println(mahasiswaPenilaian[i].nama + " | " + mataKuliahPenilaian[i].namaMK + " | Nilai Akhir: " + nilaiAkhir );
+            }
         }
-    }
+
+        public void cariMahasiswaDenganNim(Scanner sc) {
+            System.out.println("Masukkan NIM mahasiswa yang dicari: ");
+            String nim = sc.nextLine();
+            Mahasiswa m = Mahasiswa.cariMahasiswaDenganNim(nim);
+            if (m != null) {
+                System.out.println("Mahasiswa Ditemukkan");
+                m.tampilMahasiswa();
+            } else {
+                System.out.println("Mahasiswa tidak ditemukan");
+            }
+        }
+
+        public double hitungNilaiAkhir(int index) {
+            return (0.3 * nilaiTugas[index]) + (0.3 * nilaiUTS[index]) + (0.4 * nilaiUAS[index]);
+        }
+}
