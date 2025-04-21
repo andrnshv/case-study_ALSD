0354f0386d8461187eb097a9490c864f5d165ddf
aiska28 <aiskacaca8@gmail.com>
2025-04-21 15:16:20 +0700

    ubah nama

--- mahasiswa.java
+++ mahasiswa.java
@@ -1,10 +1,10 @@
-public class mahasiswa {
+public class Mahasiswa {
 
     String nim;
     String nama;
     String prodi;
 
-    public mahasiswa(String nim, String nama, String prodi) {
+    public Mahasiswa(String nim, String nama, String prodi) {
         this.nim = nim;
         this.nama = nama;
         this.prodi = prodi;
