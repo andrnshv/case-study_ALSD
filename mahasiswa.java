163054e51cbb959a344debba1a95e39f37f87e52
aiska28 <aiskacaca8@gmail.com>
2025-04-21 14:52:32 +0700

    class mahasiswa

---
+++ mahasiswa.java
@@ -0,0 +1,19 @@
+public class mahasiswa {
+
+    String nim;
+    String nama;
+    String prodi;
+
+    public mahasiswa(String nim, String nama, String prodi) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+    }
+
+    public void tampilMahasiswa () {
+        System.out.println("NIM : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Prodi : " + prodi);
+    }
+
+}
