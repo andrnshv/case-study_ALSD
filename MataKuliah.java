e967835ef357b431277bc4629b6b055a4a90477d
andriansheva12 <andriansheva8@gmail.com>
2025-04-21 14:42:26 +0700

    class Matakuliah

---
+++ MataKuliah.java
@@ -0,0 +1,14 @@
+public class MataKuliah {
+    String kodeMK, namaMK;
+    int sks;
+
+    MataKuliah(String kodeMK, String namaMK, int sks) {
+        this.kodeMK = kodeMK;
+        this.namaMK = namaMK;
+        this.sks = sks;
+    }
+
+    void tampilMatakuliah() {
+        
+    }
+}
